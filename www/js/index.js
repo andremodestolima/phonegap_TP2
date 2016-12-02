e89f061c74c4bbef6823e4a5a810b6899c087ce6
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-01 23:37:16 -0200

    v0.3

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,11 @@
+
+function teste(){}
+
+
 function pronto() {
 
     function proximaPag() {
+        navigator.notification.alert("estou aqui!!!", teste);
         navigator.vibrate(1000);
         location.href = 'pag2.html';
     }
