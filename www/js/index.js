bcae241d7224cca6212c3bd97237c03ff5d33e4f
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-03 01:06:32 -0200

    v0.3

--- www/js/index.js
+++ www/js/index.js
@@ -2,6 +2,8 @@ function pronto() {
 
     document.getElementById("proximaP").addEventListener("click", proximaPag, false);
     document.getElementById("texto").addEventListener("input", atualizarNome, false);
+    document.getElementById("salvar").addEventListener("click", salvarNome, false);
+    window.localStorage.getItem("nome");
 
     function proximaPag() {
         navigator.vibrate(1000);
@@ -12,7 +14,9 @@ function pronto() {
        document.getElementById("nome").innerHTML = document.getElementById("texto").value;
     }
 
+    function salvarNome(){
+        window.localStorage.setItem("nome",document.getElementById("texto").value)
+    }
 }
-
 
 document.addEventListener("deviceready", pronto, false);
