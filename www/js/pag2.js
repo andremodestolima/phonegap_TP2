9dbceff6e77c00fbb52928bda58d939f67393854
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-06 11:48:01 -0200

    v0.4

--- www/js/pag2.js
+++ www/js/pag2.js
@@ -15,8 +15,6 @@ function pronto() {
     }
 
     function sucessoFoto(imageData){
-        alert(imageData);
-
         window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSucesso, fileErro);  //window.TEMPORARY
 
         function fileSucesso(fs){ fs.root.getFile("fotoPerfil.img", { create: true, exclusive: false }, arquivoSucesso, alert("erro de criação de arquivo"));}
@@ -42,8 +40,29 @@ function pronto() {
         }
     }
 
-    function fileErro() {
-        alert("erro de criação de sistema");
+    function fileErro(e){
+        var msg = '';
+        switch (e.code){
+            case FileError.QUOTA_EXCEEDED_ERR:
+                msg = 'QUOTA_EXCEEDED_ERR';
+                break;
+            case FileError.NOT_FOUND_ERR:
+                msg = 'NOT_FOUND_ERR';
+                break;
+            case FileError.SECURITY_ERR:
+                msg = 'SECURITY_ERR';
+                break;
+            case FileError.INVALID_MODIFICATION_ERR:
+                msg = 'INVALID_MODIFICATION_ERR';
+                break;
+            case FileError.INVALID_STATE_ERR:
+                msg = 'INVALID_STATE_ERR';
+                break;
+            default:
+                msg = 'Unknown Error';
+                break;
+        };
+        alert('Erro: ' + msg);
     }
 
     function cameraErro(message) {
