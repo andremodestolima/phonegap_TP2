e59086061df11e6317da67eeac494fd7932f8d06
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-06 01:51:20 -0200

    v0.4

--- www/js/pag3.js
+++ www/js/pag3.js
@@ -1,17 +1,21 @@
 function pronto() {
 
     document.getElementById("voltaP").addEventListener("click", voltarPag, false);
+    document.getElementById("salvar").addEventListener("salvar", voltarPag, false);
 
     function voltarPag() {
         navigator.vibrate(200);
         location.href = 'pag2.html';
     }
 
-
-
-
-
-
+    function salvar{
+        navigator.vibrate(200);
+        navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:1});
+        function captureSuccess(mediaFile) {
+                path = mediaFile.fullPath;
+                console("Gravado!!");
+        }
+    }
 }
 
 document.addEventListener("deviceready", pronto, false);
